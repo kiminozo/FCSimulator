d4823e1e19c27b25ec4ede7ac4e3cff4c126d442
kiminozo <kiminozoeien@gmail.com>
2021-03-16 16:47:06 +0800

    cmake

--- FCLnx/main.cpp
+++ FCLnx/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     //1.初始化：解析NES文件
-    cartridge.read_from_file("../Data/Super Mario Bros.nes", 40976);
+    cartridge.read_from_file("./roms/Super Mario Bros.nes", 40976);
     //cartridge.read_from_file("../Data/yx.nes", 262288);
     //cartridge.read_from_file("../Data/Contra (U).nes", 131088);
     //2.模拟CPU（这里应该单开一个线程，每559纳秒执行一次的，但是图方便就先这么做了）
